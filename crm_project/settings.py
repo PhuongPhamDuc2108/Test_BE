498308196e12e6f2a8659d6397463c94b55adb0e
PhuongPhamDuc2108 <pducphuonghd32108@gmial.com>
2025-02-12 14:38:50 +0700

    Add .gitignore to ignore sensitive files and unnecessary files

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG') == 'True'
-ALLOWED_HOSTS = ['test-be-1.zgtf.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['test-be-1.zgtf.onrender.com']
 
 
 
