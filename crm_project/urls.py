2763ecbff25f20c4112b03bc8c7390bae3ab84c5
PhuongPhamDuc2108 <pducphuonghd32108@gmial.com>
2025-02-12 15:06:05 +0700

    Add .gitignore to ignore sensitive files and unnecessary files

--- crm_project/urls.py
+++ crm_project/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
+from rest_framework import permissions
 from core import views
 
 router = DefaultRouter()
@@ -15,13 +16,16 @@ router.register(r'employees', EmployeeViewSet)
 router.register(r'task-boards', TaskBoardViewSet)
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="CRM API",
-        default_version='v1',
-        description="API documentation for CRM",
-    ),
-    public=True,
-    permission_classes=(AllowAny,),
+   openapi.Info(
+      title="Your API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourapi.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
 )
 
 urlpatterns = [
